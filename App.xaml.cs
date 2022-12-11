fe8db2c557a84758a8dfbd24fc083956634cf49b
utratamatt <120300322+utratamatt@users.noreply.github.com>
2022-12-11 08:48:24 +0100

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Firma.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Firma
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            //ta funkcja określa co robić po ....
+            MainWindow window = new MainWindow();
+            window.DataContext = new MainWindowViewModel();//to jest powiązanie View z ViewModel
+            window.Show();
+        }
+    }
+}
